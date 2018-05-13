98571d0971e6238030344a5a98f356fbc5f2ee0c
Unknown <adai@student.ethz.ch>
2018-05-13 15:43:31 +0200

    push rest

---
+++ build/CMakeFiles/ORB_SLAM2.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/anna/slam/src/Converter.cc" "/home/anna/slam/build/CMakeFiles/ORB_SLAM2.dir/src/Converter.cc.o"
+  "/home/anna/slam/src/Frame.cc" "/home/anna/slam/build/CMakeFiles/ORB_SLAM2.dir/src/Frame.cc.o"
+  "/home/anna/slam/src/FrameDrawer.cc" "/home/anna/slam/build/CMakeFiles/ORB_SLAM2.dir/src/FrameDrawer.cc.o"
+  "/home/anna/slam/src/Initializer.cc" "/home/anna/slam/build/CMakeFiles/ORB_SLAM2.dir/src/Initializer.cc.o"
+  "/home/anna/slam/src/KeyFrame.cc" "/home/anna/slam/build/CMakeFiles/ORB_SLAM2.dir/src/KeyFrame.cc.o"
+  "/home/anna/slam/src/KeyFrameDatabase.cc" "/home/anna/slam/build/CMakeFiles/ORB_SLAM2.dir/src/KeyFrameDatabase.cc.o"
+  "/home/anna/slam/src/LocalMapping.cc" "/home/anna/slam/build/CMakeFiles/ORB_SLAM2.dir/src/LocalMapping.cc.o"
+  "/home/anna/slam/src/LoopClosing.cc" "/home/anna/slam/build/CMakeFiles/ORB_SLAM2.dir/src/LoopClosing.cc.o"
+  "/home/anna/slam/src/Map.cc" "/home/anna/slam/build/CMakeFiles/ORB_SLAM2.dir/src/Map.cc.o"
+  "/home/anna/slam/src/MapDrawer.cc" "/home/anna/slam/build/CMakeFiles/ORB_SLAM2.dir/src/MapDrawer.cc.o"
+  "/home/anna/slam/src/MapPoint.cc" "/home/anna/slam/build/CMakeFiles/ORB_SLAM2.dir/src/MapPoint.cc.o"
+  "/home/anna/slam/src/ORBextractor.cc" "/home/anna/slam/build/CMakeFiles/ORB_SLAM2.dir/src/ORBextractor.cc.o"
+  "/home/anna/slam/src/ORBmatcher.cc" "/home/anna/slam/build/CMakeFiles/ORB_SLAM2.dir/src/ORBmatcher.cc.o"
+  "/home/anna/slam/src/Optimizer.cc" "/home/anna/slam/build/CMakeFiles/ORB_SLAM2.dir/src/Optimizer.cc.o"
+  "/home/anna/slam/src/PnPsolver.cc" "/home/anna/slam/build/CMakeFiles/ORB_SLAM2.dir/src/PnPsolver.cc.o"
+  "/home/anna/slam/src/Sim3Solver.cc" "/home/anna/slam/build/CMakeFiles/ORB_SLAM2.dir/src/Sim3Solver.cc.o"
+  "/home/anna/slam/src/System.cc" "/home/anna/slam/build/CMakeFiles/ORB_SLAM2.dir/src/System.cc.o"
+  "/home/anna/slam/src/Tracking.cc" "/home/anna/slam/build/CMakeFiles/ORB_SLAM2.dir/src/Tracking.cc.o"
+  "/home/anna/slam/src/Viewer.cc" "/home/anna/slam/build/CMakeFiles/ORB_SLAM2.dir/src/Viewer.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../include"
+  "/usr/include/eigen3"
+  "/home/anna/Pangolin/include"
+  "/home/anna/Pangolin/build/src/include"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
